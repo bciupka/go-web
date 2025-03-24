0b4e6830f46cc70e891056167a5e13cef1c9b0eb
Bartłomiej Ciupka <bartek.ciupka@gmail.com>
2025-03-24 20:27:46 +0100

    auto caching concept, not ready - it runs on every request

--- 02_web_basics/04_templates_modules/pkg/render/render.go
+++ 02_web_basics/04_templates_modules/pkg/render/render.go
@@ -1,11 +1,15 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
+
+// no cache
 
 func RenderTemplateOld(w http.ResponseWriter, templateName string) {
 	renderedTemplate, _ := template.ParseFiles("./templates/"+templateName, "./templates/base.layout.tmpl")
@@ -15,9 +19,11 @@ func RenderTemplateOld(w http.ResponseWriter, templateName string) {
 	}
 }
 
+// basic caching
+
 var tc = make(map[string]*template.Template)
 
-func RenderTemplate(w http.ResponseWriter, t string) {
+func RenderTemplateBasic(w http.ResponseWriter, t string) {
 	var tmpl *template.Template
 	var err error
 
@@ -54,4 +60,66 @@ func createCachedTemplate(t string) error {
 	}
 	tc[t] = renderedTemplate
 	return nil
+}
+
+// advanced caching
+
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	tc, err := createCacheForTemplates()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Template not found")
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createCacheForTemplates() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		layouts, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(layouts) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
